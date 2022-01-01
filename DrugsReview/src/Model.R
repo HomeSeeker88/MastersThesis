680c4dc016e2e7fa2cde18ce31236c9e9564e4d2
homeseeker88 <sadkowskim@yahoo.pl>
2022-01-01 23:48:35 +0100

    installed keras and tensorflow, tokenized words from reviews and prepared them for deep learning model

--- DrugsReview/src/Model.R
+++ DrugsReview/src/Model.R
@@ -15,14 +15,23 @@ drugsCom.train <- drugsCom.train %>%
 X_train <- drugsCom.train$review
 Y_train <- drugsCom.train$rating/10
 
-X_train
-Y_train
+#X_train
+#Y_train
 
 X_train %>% str_split(" ") %>% 
   sapply(length) %>% 
   summary()
 
-num_words <- 5000
+num_words <- 10000
 max_length <- 100
+
+text_vectorization <- layer_text_vectorization(max_tokens = num_words,
+                                               output_sequence_length = max_length)
+
+text_vectorization %>% adapt(drugsCom.train$review)
+
+get_vocabulary(text_vectorization)
+
+text_vectorization(matrix(drugsCom.train$review[1], ncol = 1))
 
 #TODO: ZROBIĆ NA PECECIE
